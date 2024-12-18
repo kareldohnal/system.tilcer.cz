c08491312dfc000caca687bb570b493ea999d00e
kareldohnal <karel.dohnal@gmail.com>
2024-12-18 18:29:15 +0100

    ActionBlock.tsx

--- src/components/ActionBlock.tsx
+++ src/components/ActionBlock.tsx
@@ -1,4 +1,9 @@
-import {useState} from "react";
+import {useQuery} from "@tanstack/react-query";
+import {fetchJidloRebuild} from "../api/api.ts";
+import {useAtomValue} from "jotai/react";
+import {tokenAtom} from "../atomStore.ts";
+import {Button, Flex} from "antd";
+import "./actionBlock.scss"
 
 type Props = {
     apiCall: (token: string) => Promise<boolean>;
@@ -7,15 +12,25 @@ type Props = {
 }
 
 const ActionBlock = ({apiCall, buttonText, label}: Props) => {
-    const [loading, setLoading] = useState(false);
+    const token = useAtomValue(tokenAtom)
 
+    const { data, error, refetch, isLoading } = useQuery({
+        queryKey: ['fetchJidloRebuild'],
+        queryFn: () => apiCall(token),
+        enabled: false,
+        retry: false,
+    })
 
-
+    const handleClick = () => {
+        refetch();
+    }
+    console.log(data)
+    console.log(error)
     return (
-        <div className={"actionBlock"}>
+        <Flex justify={"space-between"} align={"center"} className={"actionBlock p-16"}>
             <div>{label}</div>
-            <button>{buttonText}</button>
-        </div>
+            <Button type={"primary"} loading={isLoading} onClick={handleClick}>{buttonText}</Button>
+        </Flex>
     );
 }
 
